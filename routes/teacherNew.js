b9255f4ef764d272cea4b7287e0fbc19db81e21e
humaira699 <30345983+humaira699@users.noreply.github.com>
2022-10-31 23:05:15 +0500

    Delete routes directory

--- routes/teacherNew.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-/* GET teacher listing. */
-router.get('/', function(req, res, next) {
-    res.send('Show teacher New dashboard');
-});
-module.exports = router;
--- routes/teachers.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-/* GET teacher listing. */
-router.get('/', function(req, res, next) {
-    res.send('Show teacher dashboard');
-});
-module.exports = router;
