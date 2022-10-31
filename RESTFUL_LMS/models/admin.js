97fab13fc57106519b69be28fde4afd9734f399f
humaira699 <30345983+humaira699@users.noreply.github.com>
2022-10-31 23:12:25 +0500

    Delete RESTFUL_LMS directory

--- RESTFUL_LMS/models/admin.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var adminSchema = new Schema({
-    name:{
-        type: String,
-        required: true
-    }
-});
-module.exports = mongoose.model('Admin',adminSchema);
